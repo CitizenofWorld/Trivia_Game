6f7c1d03cf1c6e5a606ba3b06e2f907835e447f3
Jess <jesesnz@gmail.com>
2017-11-13 14:51:45 +1100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <div className="row- 1">
+        
+       </div>
+        <div className="row- 2">
+        </div>
+        <div className="row- 3">
+        </div>
+        <div className="row- 4">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
